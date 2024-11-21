62f0d0701c0601e1bc907c4c70a7af8e847d706e
Sebastian Becker <sebastian.becker@de.bosch.com>
2024-11-21 12:26:34 +0100

    feat(service): undo changes to server
    Signed-off-by: Sebastian Becker <sebastian.becker@de.bosch.com>

--- pkg/ephemeral/server.go
+++ pkg/ephemeral/server.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2021-2024 - for information on the respective copyright owner
+// Copyright (c) 2021-2023 - for information on the respective copyright owner
 // see the NOTICE file and/or the repository https://github.com/carbynestack/ephemeral.
 //
 // SPDX-License-Identifier: Apache-2.0
@@ -232,11 +232,7 @@ func (s *Server) ActivationHandler(writer http.ResponseWriter, req *http.Request
 	select {
 	case stdout := <-s.respCh:
 		writer.WriteHeader(http.StatusOK)
-		result, err := json.Marshal(stdout)
-		if err != nil {
-			writer.Write([]byte(fmt.Sprintf("error marshalling the result: %s", err.Error())))
-		}
-		writer.Write(result)
+		writer.Write(stdout)
 	case err := <-s.errCh:
 		msg := fmt.Sprintf("error while talking to Discovery: %s", err)
 		writer.WriteHeader(http.StatusInternalServerError)
